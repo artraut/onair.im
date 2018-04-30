aad5c022c78d799a876b849301fb6d45a7a3d71b
artraut <info@artraut.com>
2018-04-30 17:13:02 +0300

    Update mtproto.js

--- app/js/lib/mtproto.js
+++ app/js/lib/mtproto.js
@@ -33,7 +33,7 @@ angular.module('izhukov.mtproto', ['izhukov.utils'])
 
         if (Config.Modes.ssl || !Config.Modes.http) {
           var subdomain = sslSubdomains[dcID - 1] + (upload ? '-1' : '')
-          var path = Config.Modes.test ? 'apiw_test1' : 'apiw1/'
+          var path = Config.Modes.test ? 'apiw_test1' : '/apiw1'
           chosenServer = 'https://onair.im/' + subdomain + path
           return chosenServer
         }
